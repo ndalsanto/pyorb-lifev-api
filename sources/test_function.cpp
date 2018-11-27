76ec95069880fc2b27d2bdaef6586e902adb125b
Niccolo' Dal Santo <ncl.dalsanto@gmail.com>
2018-11-27 13:07:38 +0100

    linked LifeV with also the test library

--- sources/test_function.cpp
+++ sources/test_function.cpp
@@ -1,7 +1,11 @@
 #include "test_function.hpp"
+#include <lifev/core/LifeV.hpp>
+#include <lifev/core/util/LifeChronoManager.hpp>
 
 int test_function( int a )
 {
     std::cout << "This is a test to show a " << a << std::endl;
-}
 
+    LifeV::LifeChrono globalChrono;
+
+}
--- test/loadlifev.cpp
+++ test/loadlifev.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include "test_function.hpp"
-       
 
 #include <lifev/core/LifeV.hpp>
 #include <lifev/core/util/LifeChronoManager.hpp>
 
- 
 int main (int argc, char **argv)
 {
 //	std::cout << "LIFEV version: " << LIFEV_VERSION_MAJOR
 //		  << "." << LIFEV_VERSION_MINOR << std::endl;
 
-    std::cout << "Ciao" << std::endl;
+    std::cout << "Hello world!" << std::endl;
 
     LifeV::LifeChrono globalChrono;
 
 
     test_function( 3 );
 
-    return 0; 
+    return 0;
 }
-
