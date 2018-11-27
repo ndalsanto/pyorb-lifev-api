55defa1e768805316bd0536ffbdaf51d8d84b62c
Niccolo' Dal Santo <ncl.dalsanto@gmail.com>
2018-11-27 11:24:49 +0100

    added lifev as external library

--- test/loadlifev.cpp
+++ test/loadlifev.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include "test_function.hpp"
-// #include <rb/reduced_basis/fem/FemProblem.hpp>
-        
+       
+
+#include <lifev/core/LifeV.hpp>
+#include <lifev/core/util/LifeChronoManager.hpp>
+
+ 
 int main (int argc, char **argv)
 {
 //	std::cout << "LIFEV version: " << LIFEV_VERSION_MAJOR
 //		  << "." << LIFEV_VERSION_MINOR << std::endl;
 
-	std::cout << "Ciao" << std::endl;
+    std::cout << "Ciao" << std::endl;
 
-	test_function( 3 );
+    LifeV::LifeChrono globalChrono;
 
-	return 0; 
+
+    test_function( 3 );
+
+    return 0; 
 }
 
